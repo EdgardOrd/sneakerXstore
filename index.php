9f172c39df41045cc7beac8c92486d68b9fa4516
EdgardOrd <edgard.ordonez26@gmail.com>
2023-04-24 22:43:15 -0600

    Cambios en el filtro por talla

--- index.php
+++ index.php
@@ -47,7 +47,8 @@ if (isset($_POST['add'])) {
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Shopping Cart</title>
 
@@ -55,7 +56,8 @@ if (isset($_POST['add'])) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css" />
 
     <!-- Bootstrap CDN -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <link rel="stylesheet" href="style.css">
 </head>
@@ -68,20 +70,35 @@ if (isset($_POST['add'])) {
         <div class="row py-3">
             <div class="container text-right">
                 <Span class="mr-5">Tallas</Span>
-                <select class="custom-select col-3">
-                    <?php
-                    $result = $database->getTallas();
+                <div class="dropdown">
+                    <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                        aria-expanded="false">
+                        Seleccione una talla
+                    </a>
+
+                    <div class="dropdown-menu">
+                        <?php
+                        $result = $database->getTallas();
+                        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                            echo "<a class='dropdown-item' href='index.php?id={$row['talla']}'>{$row['talla']}</a>";
+                        }
+                        ?>
+                        
+                    </div>
+                </div>
+                <!-- <select class="custom-select col-3"> -->
+                    <?php 
+                   /* $result = $database->getTallas();
                     while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
                         echo "<option value='{$row['talla']}'>{$row['talla']}</option>";
-                    }
-                    ?>
-                    <!-- <option value="1">One</option> -->
-                </select>
+                    }*/
+                    ?> 
+                <!-- </select> -->
             </div>
         </div>
         <div class="row text-center py-3">
             <?php
-            $result = $database->getInStock(9);
+            $result = $database->getInStock($_GET["id"]);
             while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
                 component($row['nombre'], $row['precio'], $row['imagen'], $row['id']);
             }
@@ -93,9 +110,15 @@ if (isset($_POST['add'])) {
 
 
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
