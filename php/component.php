c0fef32ec51ba0c9ad575f2c2377048d4f77f37b
EdgardOrd <edgard.ordonez26@gmail.com>
2023-04-28 17:20:51 -0600

    Agregando tallas y cantidad en el carrito

--- php/component.php
+++ php/component.php
@@ -65,6 +65,7 @@ function component($productname, $productprice, $productimg, $productid){
                                 <h5>
                                     <small><s class=\"text-secondary\">$$old</s></small>
                                     <span class=\"price\">$$productprice</span>
+                                    
                                 </h5>
 
                                 <button type=\"submit\" class=\"btn btn-warning my-3\" name=\"add\">Add to Cart <i class=\"fas fa-shopping-cart\"></i></button>
@@ -78,25 +79,33 @@ function component($productname, $productprice, $productimg, $productid){
     echo $element;
 }
 
-function cartElement($productimg, $productname, $productprice, $productid){
+function cartElement($productimg, $productname, $productprice, $productid,$cantidad,$talla){
     $element = "
     
     <form action=\"cart.php?action=remove&id=$productid\" method=\"post\" class=\"cart-items\">
-                    <div class=\"border rounded\">
-                        <div class=\"row bg-white p-3\">
-                            <div class=\"col-md-3 pl-0\">
-                                <img src=$productimg alt=\"Image1\" class=\"img-fluid\">
-                            </div>
-                            <div class=\"col-md-6\">
-                                <h5 class=\"pt-2\">$productname</h5>
-                                <small class=\"text-secondary\">Vendido por: sneakerXstore</small>
-                                <h5 class=\"pt-2\">$$productprice</h5>
-                                <button type=\"submit\" class=\"btn btn-warning\">Guardar para después</button>
-                                <button type=\"submit\" class=\"btn btn-danger mx-2\" name=\"remove\">Eliminar del carrito</button>
-                            </div>
-                        </div>
-                    </div>
-                </form>
+    <div class=\"border rounded\">
+        <div class=\"row bg-white p-3\">
+            <div class=\"col-md-3 pl-0\">
+                <img src=$productimg alt=\"Image1\" class=\"img-fluid\">
+            </div>
+            <div class=\"col-md-6\">
+                <h5 class=\"pt-2\">$productname</h5>
+                <small class=\"text-secondary\">Vendido por: sneakerXstore</small>
+                <div class=\"d-flex align-items-center mb-4 w-100\">
+                    <h5 class=\"pt-2\">$$productprice</h5>
+                    <label for=\"cantidad\" class=\"txt-dark mx-3\">Cantidad:</label>
+                    <input type=\"number\" id=\"cantidad\" class=\"form-control w-25 d-inline\" name=\"cantidad\" value=\"1\" min=\"1\" max=\"$cantidad\">
+                    <label for=\"talla\" class=\"txt-dark mx-3\">Talla:</label>
+                    <input type=\"text\" id=\"talla\" class=\"form-control w-25 d-inline\" name=\"talla\" value=\"$talla\" readonly>
+                </div>
+
+                <button type=\"submit\" class=\"btn btn-warning\">Guardar para después</button>
+                <button type=\"submit\" class=\"btn btn-danger mx-2\" name=\"remove\">Eliminar del carrito</button>
+            </div>
+        </div>
+    </div>
+    </form>
+
     
     ";
     echo  $element;
